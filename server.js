0dea50726d84876129eb7320300fdd0714ee0b3e
megzimo <40956115+megzimo@users.noreply.github.com>
2018-11-02 15:50:39 -0500

    updating port info

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require('path');
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 
 app.use(bodyParser.urlencoded({ extended:true }));
